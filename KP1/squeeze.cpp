2b196009d1aedd4389b11abb33e4293b72bd79bb
egorix2000 <egorix2000@mail.ru>
2018-10-29 12:26:10 +0300

    KP1

---
+++ KP1/squeeze.cpp
@@ -0,0 +1,148 @@
+﻿/**
+Вариант 1
+Дана матрица размера m x n. "Уплотнить" ее, удаляя из нее строки и столбцы, заполненные нулями.
+Быченок Егор
+
+tests in folder "tests"
+**/
+
+#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <cstring>
+#include "FileLib.h"
+
+using namespace std;
+
+const int MAX_STRING_LENGTH = 1024;
+//change file paths to yours
+const char* INPUT_FILE = "Downloads/A/tests/squeeze/input1.txt";
+const char* OUTPUT_FILE = "Downloads/A/tests/squeeze/output1.txt";
+
+
+int printTwoDimensionalArray(int **a, int n, int m, ofstream& fout){
+    for (int t = 0; t < n; t++){
+        for (int i = 0; i < m; i++){
+            fout << a[t][i] << " ";
+        }
+        fout << endl;
+    }
+    return 0;
+}
+
+int** deleteRow(int** a, int& n, int& m, int iRow){
+    int** newA = new int*[n-1];
+    for (int i = 0; i < n-1; i++){
+        newA[i] = new int[m];
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            if (i < iRow){
+                newA[i][j] = a[i][j];
+            } else if(i > iRow) {
+                newA[i-1][j] = a[i][j];
+            }
+        }
+    }
+    n--;
+    return newA;
+}
+
+int** deleteColumn(int** a, int& n, int& m, int iColumn){
+    int** newA = new int*[n];
+    for (int i = 0; i < n; i++){
+        newA[i] = new int[m-1];
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            if (j < iColumn){
+                newA[i][j] = a[i][j];
+            } else if(j > iColumn) {
+                newA[i][j-1] = a[i][j];
+            }
+        }
+    }
+    m--;
+    return newA;
+}
+
+int enterTwoDimensionalArrayFromFile(int** a, int n, int m, ifstream& fin){
+    for (int t = 0; t < n; t++){
+        for (int i = 0; i < m; i++){
+            fin >> a[t][i];
+        }
+    }
+    return 0;
+}
+
+int** removeAllZeroRows(int** a, int &n, int &m){
+    bool isZeroRow;
+    for (int t = 0; t < n; t++){
+        isZeroRow = true;
+        for (int i = 0; i < m; i++){
+            if (a[t][i] != 0){
+                isZeroRow = false;
+                break;
+            }
+        }
+        if (isZeroRow){
+            a = deleteRow(a, n, m, t);
+            t--;
+        }
+    }
+
+    return a;
+}
+
+int** removeAllZeroColumns(int** a, int &n, int &m){
+    bool isZeroColumn;
+    for (int t = 0; t < m; t++){
+        isZeroColumn = true;
+        for (int i = 0; i < n; i++){
+            if (a[i][t] != 0){
+                isZeroColumn = false;
+                break;
+            }
+        }
+        if (isZeroColumn){
+            a = deleteColumn(a, n, m, t);
+            t--;
+        }
+    }
+    return a;
+}
+
+int main(){
+    ifstream fin;
+    ofstream fout;
+    char error[MAX_STRING_LENGTH];
+
+    int n;
+    int m;
+    int** a;
+
+    fin.open(INPUT_FILE);
+
+    if(validateFile(fin, error)){
+        fout.open(OUTPUT_FILE);
+
+        fin >> n >> m;
+
+        a = new int*[n];
+        for (int i = 0; i < n; i++){
+            a[i] = new int[m];
+        }
+
+        enterTwoDimensionalArrayFromFile(a, n, m, fin);
+
+        a = removeAllZeroRows(a, n, m);
+        a = removeAllZeroColumns(a, n, m);
+        printTwoDimensionalArray(a, n, m, fout);
+    } else {
+        cout << error << endl;
+    }
+
+    fin.close();
+    fout.close();
+    return 0;
+}
---
+++ KP1/vowelFractionSort.cpp
@@ -0,0 +1,117 @@
+﻿/**
+Вариант 1
+Дан текст, состоящий из предложений. Найти слово, которое встречается во всех предложениях текста, или сообщить, что такого слова нет.
+Быченок Егор
+
+tests in folder "tests"
+**/
+
+#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <cstring>
+#include "FileLib.h"
+
+using namespace std;
+
+const int MAX_STRING_LENGTH = 1024;
+const int MAX_WORD_COUNT = 1024;
+const char VOVELS[] = "eyuioaEYUIOA";
+
+//change file paths to yours
+const char* INPUT_FILE = "Downloads/A/tests/vowelFractionSort/input1.txt";
+const char* OUTPUT_FILE = "Downloads/A/tests/vowelFractionSort/output1.txt";
+
+struct wordWithVovelFraction{
+    char word[MAX_STRING_LENGTH];
+    double vovelFraction;
+};
+
+bool isVovel(char c){
+    bool isVovel = false;
+    if (strchr(VOVELS, c)){
+        isVovel = true;
+    }
+    return isVovel;
+}
+
+double findVowelFraction(char* s){ //находим долю гласных
+    double fraction = 0;
+    for (int i = 0; i < strlen(s); i++){
+        if (isVovel(s[i])){
+            fraction++;
+        }
+    }
+
+    fraction /= strlen(s);
+    return fraction;
+}
+
+bool comparator(wordWithVovelFraction a, wordWithVovelFraction b){
+    if (a.vovelFraction > b.vovelFraction){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+void swapElements(wordWithVovelFraction &a, wordWithVovelFraction& b){
+    wordWithVovelFraction *temp = new wordWithVovelFraction;
+    *temp = a;
+    a = b;
+    b = *temp;
+    delete temp;
+}
+
+
+void sortArray(wordWithVovelFraction *a, int n){
+    int minIndex;
+
+    for (int t = 0; t < n-1; t++){
+        minIndex = t;
+        for (int i = t+1; i < n; i++){
+            if (comparator(a[minIndex], a[i])){
+                minIndex = i;
+            }
+        }
+        if (minIndex != t){
+            swapElements(a[t], a[minIndex]);
+        }
+    }
+}
+int main()
+{
+    ifstream fin;
+    ofstream fout;
+    char error[MAX_STRING_LENGTH];
+
+    wordWithVovelFraction words[MAX_WORD_COUNT];
+    int wordsCount = 0;
+
+
+    fin.open(INPUT_FILE);
+
+    if(validateFile(fin, error)){
+        fout.open(OUTPUT_FILE);
+
+        while (fin >> words[wordsCount].word){ // ввод
+            wordsCount++;
+        }
+
+        for (int i = 0; i < wordsCount; i++){
+            words[i].vovelFraction = findVowelFraction(words[i].word);
+        }
+
+        sortArray(&words[0], wordsCount);
+
+        for (int i = 0; i < wordsCount; i++){
+            fout << words[i].word << endl;
+        }
+    } else {
+        cout << error << endl;
+    }
+
+    fin.close();
+    fout.close();
+    return 0;
+}
